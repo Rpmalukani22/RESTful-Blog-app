3a4c2ffe7be08dca360cf081850e9976564f6e9a
ruchiteshmalukani <ruchiteshmalukani@gmail.com>
2018-09-27 09:43:17 +0000

    All 7 Rest Routes are added

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 var express=require('express');
 var bodyParser=require("body-parser");
 var mongoose=require("mongoose");
-
+var methodOverride = require('method-override');
 var app = express();
 
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({extended:true}));
+app.use(methodOverride('_method'));
 app.set("view engine","ejs");
 mongoose.connect("mongodb://localhost/blogdb",{ useNewUrlParser: true });
 
@@ -60,6 +61,40 @@ app.post('/blogs',(req,res)=>{
         res.redirect('/blogs');
     });
     });
+app.get('/blogs/:id/edit',(req,res)=>{
+   Blog.findById(req.params.id,(err,foundObj)=>{
+       if(err)
+       {
+           app.redirect('/blogs');
+       }
+       else{
+           res.render('edit',{currentBlog:foundObj}); 
+       }
+   });
+    
+   
+});
+app.put('/blogs/:id',(req,res)=>{
+  Blog.findOneAndUpdate({_id: req.params.id},req.body.updatedBlog,(err,updatedObj)=>{
+      if(err){
+          console.log("update error!!!!!");
+          res.redirect('/blogs/'+req.params.id);
+      }
+      else{
+          res.redirect('/blogs/'+req.params.id);
+      }
+  });
+});
+app.delete('/blogs/:id',(req,res)=>{
+  Blog.findOneAndDelete({ _id: req.params.id},(err,updatedObj)=>{
+      if(err){
+          res.redirect('/blogs/');
+      }
+      else{
+          res.redirect('/blogs/');
+      }
+  });
+});
 app.listen(process.env.PORT,process.env.IP,()=>{
    console.log("Server started!"); 
 });
